3ad3e0bbe028d83b932e51aefb5ebcf13ae83f4b
Zombaya01 <1260916-Zombaya01@users.noreply.gitlab.com>
2022-02-10 23:59:53 +0100

    Add simple unittest

---
+++ demo/symfony4.4/tests/SecretTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\Secret;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class SecretTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        self::bootKernel([]);
+    }
+
+    public function testSecretsAreEncryptedInDatabase()
+    {
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = self::$container->get('doctrine.orm.entity_manager');
+
+        // Make sure we do not store testdata
+        $entityManager->beginTransaction();
+
+        $name = 'test123';
+        $secretString = 'i am a secret string';
+
+        // Create entity to test with
+        $secret = (new Secret())
+            ->setName($name)
+            ->setSecret($secretString);
+
+        $entityManager->persist($secret);
+        $entityManager->flush();
+
+
+        $secretRepository = $entityManager->getRepository(Secret::class);
+
+
+
+
+        $qb = $secretRepository->createQueryBuilder('s');
+        $qb->select('s')
+            ->addSelect('(s.secret) as rawSecret')
+            ->where('s.name = :name')
+            ->setParameter('name',$name)
+            ->orderBy('s.name','ASC');
+        $result = $qb->getQuery()->getSingleResult();
+
+        $actualSecretObject = $result[0];
+        $actualRawSecret = $result['rawSecret'];
+
+        self::assertEquals($secret->getSecret(), $actualSecretObject->getSecret());
+        self::assertEquals($secret->getName(), $actualSecretObject->getName());
+        // Make sure it is encrypted
+        self::assertNotEquals($secret->getSecret(),$actualRawSecret);
+    }
+}
